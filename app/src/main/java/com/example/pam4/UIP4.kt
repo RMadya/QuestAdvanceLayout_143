0ac0f493778d262c282a418bc05cb12cacf052cd
RMadya <kereyoben75@gmail.com>
2025-10-23 16:33:12 +0700

    untuk memberikan atau menampilkan gambar secara horizontal

--- app/src/main/java/com/example/pam4/UIP4.kt
+++ app/src/main/java/com/example/pam4/UIP4.kt
@@ -44,3 +44,16 @@ fun ActivitasPertama(modifier: Modifier){
         fontSize = 22.sp
         )
         Spacer(modifier = Modifier.height(height = 25.dp))
+        Card(
+            modifier = Modifier
+                .fillMaxWidth(fraction = 1f)
+                .padding(all = 12.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = Color.DarkGray
+            )
+        ) {
+            Row() {
+                val gambar = painterResource(id = R.drawable.luniv)
+                Image(
+                    painter = gambar,
+                    contentDescription = null,
