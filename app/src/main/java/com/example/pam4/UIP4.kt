8be492ecf9ec7bf7001a10b48032bce7cc7741b0
RMadya <kereyoben75@gmail.com>
2025-10-23 16:25:27 +0700

    deklarasi fungsi ActivitasPertama (seharusnya memiliki anotasi @Composable di atasnya agar bisa merender UI)

--- app/src/main/java/com/example/pam4/UIP4.kt
+++ app/src/main/java/com/example/pam4/UIP4.kt
@@ -29,8 +29,4 @@ import androidx.compose.ui.unit.sp
 
 @Composable
 fun ActivitasPertama(modifier: Modifier){
-    Column(modifier= Modifier
-        .padding(top=100.dp)
-        .fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally
-    )
+
